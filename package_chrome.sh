32421c73fad24f482d8f99875cf37d1634cac533
Ben Sarmiento <ben.sarmiento@forto.com>
2023-11-05 13:26:10 +0100

    Fix packagers

--- package_chrome.sh
+++ package_chrome.sh
@@ -3,15 +3,15 @@
 # Define the directory where the extension files are located
 EXTENSION_DIR="$(pwd)/chrome"
 
-cp -r "$(pwd)/icons" "$EXTENSION_DIR"
-cp -r "$(pwd)/background.js" "$EXTENSION_DIR"
-cp -r "$(pwd)/content_script.js" "$EXTENSION_DIR"
+cp -fr "$(pwd)/icons" "$EXTENSION_DIR"
+cp -fr "$(pwd)/background.js" "$EXTENSION_DIR"
+cp -fr "$(pwd)/content_script.js" "$EXTENSION_DIR"
 
 # Define the output directory for the packaged extension
 OUTPUT_DIR="$EXTENSION_DIR/web-ext-artifacts"
 
 # Package the extension
 echo "Packaging the extension..."
-web-ext build --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --ignore-files="create_icons.sh logo.svg *.md"
+web-ext build --overwrite-dest --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --ignore-files="create_icons.sh logo.svg *.md"
 
 echo "Extension package created in $OUTPUT_DIR"
--- package_firefox.sh
+++ package_firefox.sh
@@ -3,20 +3,20 @@
 # Define the directory where the extension files are located
 EXTENSION_DIR="$(pwd)/firefox"
 
-cp -r "$(pwd)/icons" "$EXTENSION_DIR"
-cp -r "$(pwd)/background.js" "$EXTENSION_DIR"
-cp -r "$(pwd)/content_script.js" "$EXTENSION_DIR"
+cp -fr "$(pwd)/icons" "$EXTENSION_DIR"
+cp -fr "$(pwd)/background.js" "$EXTENSION_DIR"
+cp -fr "$(pwd)/content_script.js" "$EXTENSION_DIR"
 
 # Define the output directory for the packaged extension
 OUTPUT_DIR="$EXTENSION_DIR/web-ext-artifacts"
 
 # Package the extension
 echo "Packaging the extension..."
-web-ext build --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --ignore-files="create_icons.sh logo.svg *.md"
+web-ext build --overwrite-dest --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --ignore-files="create_icons.sh logo.svg *.md"
 
 echo "Extension package created in $OUTPUT_DIR"
 
-source credentials.sh
+source firefox_credentials.sh
 
 # Check if AMO_JWT_ISSUER and AMO_JWT_SECRET are set
 if [ -z "$AMO_JWT_ISSUER" ] || [ -z "$AMO_JWT_SECRET" ]; then
@@ -25,7 +25,7 @@ if [ -z "$AMO_JWT_ISSUER" ] || [ -z "$AMO_JWT_SECRET" ]; then
 fi
 
 # Sign the extension
-echo "Signing the extension..."
-web-ext sign --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --api-key="$AMO_JWT_ISSUER" --api-secret="$AMO_JWT_SECRET"
+# echo "Signing the extension..."
+# web-ext sign --source-dir="$EXTENSION_DIR" --artifacts-dir="$OUTPUT_DIR" --api-key="$AMO_JWT_ISSUER" --api-secret="$AMO_JWT_SECRET"
 
-echo "Extension signed. Artifacts are in $OUTPUT_DIR"
+# echo "Extension signed. Artifacts are in $OUTPUT_DIR"
